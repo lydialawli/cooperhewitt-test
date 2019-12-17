dd83833c41fe9a1b3615900b5c044ff2622c0d1f
lydialawli <lydialawli@gmail.com>
2019-12-17 15:59:57 +0100

    add loader/spinner

--- App.js
+++ App.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react';
 import { ACCESS_TOKEN } from 'react-native-dotenv';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, ActivityIndicator,
+  FlatList, TouchableOpacity
+} from 'react-native';
 
 // ApiClient.init(ACCESS_TOKEN)
 
 export default class App extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "List of Exhibitions",
+      headerStyle: { backgroundColor: "#fff" },
+      headerTitleStyle: { textAlign: "center", flex: 1 }
+    }
+  }
   constructor(props) {
     super(props);
     this.state = {
       loading: true,
-      dataSource: []
-    };
+      exhibitions: []
+    }
   }
 
   componentDidMount() {
     fetch(`https://api.collection.cooperhewitt.org/rest/?method=cooperhewitt.exhibitions.getList&access_token=${ACCESS_TOKEN}&page=1&per_page=100`)
       .then(response => response.json())
       .then((res) => {
-        console.log('data is: ', res)
+        // console.log('data is: ', res)
         this.setState({
           loading: false,
-          dataSource: res
+          exhibitions: res
         })
       })
       .catch(error => console.log(error))
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="#0c9" />
+        </View>
+      )
+    }
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
 
       </View>
-    );
+    )
   }
 }
 
@@ -48,9 +65,10 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  loader: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
   },
 });
